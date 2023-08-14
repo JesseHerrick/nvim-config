dbb8432b995f840d802086deabefe11fc2fa54f8
Jesse Herrick <me@jesseherrick.com>
2023-08-14 19:40:02 -0400

    Tab changes

--- lua/jesseherrick/settings.lua
+++ lua/jesseherrick/settings.lua
@@ -4,8 +4,10 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.swapfile = false
-vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
+
+vim.opt.shiftwidth = 0
+vim.opt.tabstop = 4
 
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
@@ -17,4 +19,3 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "120"
-
