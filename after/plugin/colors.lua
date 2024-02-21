4d94e247d01c2a014bb84e6132221fa0cce4514e
Jesse Herrick <me@jesseherrick.com>
2024-02-21 10:16:19 -0500

    Add color correction for Elixir modules

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,23 +1,14 @@
-function SetColorSchema(color)
-	color = color or "gruvbox-material"
-end
-
 vim.g.gruvbox_material_foreground = 'mix'
 vim.g.gruvbox_material_background = 'hard'
 
-local grpid = vim.api.nvim_create_augroup('custom_highlights_gruvboxmaterial', {})
-
+local grpid = vim.api.nvim_create_augroup('custom_highlights_gruvbox-material', {})
 vim.api.nvim_create_autocmd('ColorScheme', {
 	group = grpid,
 	pattern = 'gruvbox-material',
 	callback = function()
-		local function hl_lnk(src, trgt)
-			vim.api.nvim_set_hl(0, src, { link = trgt })
-		end
-
-		-- see: https://github.com/sainnhe/gruvbox-material/blob/master/colors/gruvbox-material.vim
-		hl_lnk('@symbol.elixir', 'Yellow')
-		hl_lnk('@constant.elixir', 'Aqua')
+		-- ':TSHighlightCapturesUnderCursor' will tell the correct match to use for color correction.
+		vim.cmd('highlight! link @module.elixir AquaBold')
+		vim.cmd('highlight! link @constant Purple')
 	end
 })
 
